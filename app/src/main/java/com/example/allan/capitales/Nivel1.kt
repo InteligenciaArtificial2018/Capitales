1e78518712e1e936e844880f94e980bad9154306
Phantom <mynameisallan93@gmail.com>
2018-11-25 15:50:02 -0600

    Agregue los textview de record

--- app/src/main/java/com/example/allan/capitales/Nivel1.kt
+++ app/src/main/java/com/example/allan/capitales/Nivel1.kt
@@ -3,6 +3,7 @@ package com.example.allan.capitales
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 import android.widget.Button
 
 class Nivel1 : AppCompatActivity() {
@@ -10,6 +11,11 @@ class Nivel1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nivel1)
+
+
+
+
+
 
         val TAG = "com.example.allan.Nive1.MULTIPLE"
 
@@ -22,3 +28,4 @@ class Nivel1 : AppCompatActivity() {
         }
     }
 }
+
