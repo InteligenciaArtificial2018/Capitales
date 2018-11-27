aa7983139c74ea21897d3bf0f19e024f15855771
Phantom <mynameisallan93@gmail.com>
2018-11-26 23:25:50 -0600

    Creacion pantalla Seis Nivel 1

---
+++ app/src/main/java/com/example/allan/capitales/SeisNivel1.kt
@@ -0,0 +1,12 @@
+package com.example.allan.capitales
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SeisNivel1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seis_nivel1)
+    }
+}
