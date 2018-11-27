01e60dcaa8c20b50b42216cc8939a9e787073a54
Phantom <mynameisallan93@gmail.com>
2018-11-27 08:43:31 -0600

    Creacion de la pantalla Juego de Tiempo

---
+++ app/src/main/java/com/example/allan/capitales/JuegoTiempoNivel1.kt
@@ -0,0 +1,12 @@
+package com.example.allan.capitales
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class JuegoTiempoNivel1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_juego_tiempo_nivel1)
+    }
+}
