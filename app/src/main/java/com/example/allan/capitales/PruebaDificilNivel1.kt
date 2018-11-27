29f5f5c19469e08623f402b9dce470f66fbbf007
Phantom <mynameisallan93@gmail.com>
2018-11-27 09:19:55 -0600

    Creacion de la pantalla Prueba Dificil del Nivel1

---
+++ app/src/main/java/com/example/allan/capitales/PruebaDificilNivel1.kt
@@ -0,0 +1,12 @@
+package com.example.allan.capitales
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PruebaDificilNivel1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prueba_dificil_nivel1)
+    }
+}
